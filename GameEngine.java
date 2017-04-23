db963f9e738ff0256c27295eb0752cc79ec29605
JonasKlare <JonasKlare@users.noreply.github.com>
2017-04-23 11:03:25 -0600

    GameEngine
    Unfinished version of GameEngine that attempts to create the display and listen for action listeners.

---
+++ GameEngine.java
@@ -0,0 +1,101 @@
+package recyclean;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class GameEngine extends Component{
+
+	//Variables
+	private boolean event = false, t = false, r = false, space = false;
+	BufferedImage trash, recycle;
+	Bucket[] bins;
+	
+	//Variables
+	
+	//Constructor
+	public GameEngine()
+	{
+		//try to get images in there. 
+		try {
+			trash = ImageIO.read(new File("Trash Bin.png"));
+			recycle = ImageIO.read(new File("Recycle Bin.png"));
+		} catch (IOException e) {
+		}
+		
+		Sidebar side = new Sidebar();
+		Bucket[] bins = new Bucket[5];
+		boolean[] arr = {true, false, false, false, false}; 
+		Coordinate loc = new Coordinate(Config.SCREEN_WIDTH/3 , Config.SCREEN_HEIGHT/2);
+		Coordinate size = new Coordinate(0, 0); //TODO change to be the actual size of the image.
+			bins[0] = new Bucket(arr, loc, size, trash);
+		//Shift around the vars to allow for bucket 2. 
+		arr[0] = false; arr[1] = true;
+		loc.incrementX(Config.SCREEN_WIDTH/3);
+			bins[1] = new Bucket(arr, loc, size, recycle);
+	}
+	//Constructor
+	
+	//Methods
+	public void paint(Graphics g)
+	{
+		//Draw all of the non-null bins. 
+		g.drawImage(bins[0].getImage(), bins[0].getLocation().getX(), bins[0].getLocation().getY(), null);
+	}
+	public Dimension getPreferredSize()
+	{
+		return new Dimension(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+	}
+	
+	private void loop()
+	{
+		if( true/*//TODO Any keyaction event occurs*/ ) event = true;
+		if(event)
+		{
+			//T is pressed
+			if(t)
+			{
+				
+			}
+			//R is pressed
+			if(r)
+			{
+				
+			}
+			
+			//Space is pressed for the pause button. 
+			if(space)
+			{
+				
+			}
+		}
+			
+		//Set event to be false at the end of the loop. 
+		event = false; 
+	}
+	
+	public static void main(String[] args) 
+	{
+		JFrame f = new JFrame("Short Worm Memory");
+        
+        f.addWindowListener(new WindowAdapter(){
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+
+        f.add(new GameEngine());
+        f.pack();
+        f.setVisible(true);
+	}
+
+}
